08445838e82da07ba7195c5530fc28f5271aaefa
Leonid Solovev <ls3112@yandex.ru>
2023-04-09 11:22:01 +0300

    Created

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число: ");
+string inputA = Console.ReadLine() ?? "";
+int numberA = int.Parse(inputA);
+
+Console.Write($"Введите квадрат числа {numberA} : ");
+string inputB = Console.ReadLine() ?? "";
+int numberB = int.Parse(inputB);
+
+int sqr = numberA*numberA;
+
+if (numberB == sqr){
+    Console.WriteLine("Верно!");
+}
+else{
+    
+    Console.WriteLine($"Нет! Квадрат числа {numberA} - {sqr}");
+}
